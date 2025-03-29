fda625673690e3b0231facd558afc51b2f99d4ff
danhsaliem <danhliem2004@gmail.com>
2025-03-29 08:27:58 +0700

    commit

---
+++ DanhSaLiem-2122110589/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace DanhSaLiem_2122110589.Model
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Image { get; set; }
+    }
+}
